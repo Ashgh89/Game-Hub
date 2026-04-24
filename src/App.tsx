f45dea4bcd6ec01b20d85c975fa2ac8139ef7248
Ashgh89 <ghaneiash@gmail.com>
2026-04-24 10:51:02 +0200

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,27 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
-      <Button>Click me</Button>
-      <Button variant="solid">Solid</Button>
-      <Button variant="subtle">Subtle</Button>
-      <Button variant="surface">Surface</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="plain">Plain</Button>
-
-      <Button colorPalette="yellow">Change Color</Button>
-      <Button colorPalette="blue">Change Color</Button>
-      <Button colorPalette="blue" variant="subtle">
-        light blue
-      </Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav  nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
+        aside
+      </GridItem>
+      {/* Even shorter (using display shorthands added in v3) => hideBelow="lg" */}
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
   );
 }
 
 export default App;
-
-// For Styling we gonna use Chakra UI
-// - Go to chakra-ui.com
-// - Start Building -> Vite
-// - Copy in terminal -> npm i @chakra-ui/react @emotion/react
-// - Copy in Terminal -> npx @chakra-ui/cli snippet add (to build your UI faster)
-// - Copy in main.tsx ->import { Provider } from "@/components/ui/provider"
-// - Wrap <App/> into <Provider></Provider> => <Provider><App/></Provider>
-// - Copy ->  "paths": {"@/*": ["./src/*"]} in tsconfig.app.json
-// - Copy in Terminal -> npm i -D vite-tsconfig-paths
-// - Copy -> tsconfigPaths() like -> plugins: [react(), tsconfigPaths()], in vite.config.ts
-// - And import -> import tsconfigPaths from "vite-tsconfig-paths" in vite.config.ts
-// - Now go to Components in chakra Website and ENJOY!
